c51f5b14fd5c174fa663466f52ec4a219e14cb40
cebarbosa <carlos.barbosa@usp.br>
2018-10-19 12:37:05 -0300

    Included data directory for M85.

--- M85/context.py
+++ M85/context.py
@@ -8,14 +8,18 @@ Author : Carlos Eduardo Barbosa
 Context file for the work on galaxy M85.
 
 """
+import os
 import getpass
+
 import astropy.units as u
 import matplotlib.pyplot as plt
 
 if getpass.getuser() == "kadu":
     home = "/home/kadu/Dropbox/WIFIS/M85"
 else:
-    home = "/sto/home/cebarbosa/bsf-sim"
+    home = "/sto/home/cebarbosa/WIFIS/M85"
+
+data_dir = os.path.join(home, "data")
 
 plt.style.context("seaborn-paper")
 plt.rcParams["text.usetex"] = True
