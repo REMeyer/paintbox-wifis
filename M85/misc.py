85ae6e765613e14b45ea5ff8cd121188ec77f9cc
cebarbosa <carlos.barbosa@usp.br>
2018-10-22 17:17:33 -0300

    Included DER_SNR function.

--- M85/misc.py
+++ M85/misc.py
@@ -13,3 +13,14 @@ import numpy as np
 def calc_wave(header, naxis=1):
     wave = ((np.arange(header['NAXIS3']) + 1
              - header['CRPIX3']) * header['CDELT3'] + header['CRVAL3'])
+
+
+def snr(flux, axis=0):
+    """ Calculates the S/N ratio of a spectra.
+
+    Translated from the IDL routine der_snr.pro """
+    signal = np.nanmedian(flux, axis=axis)
+    noise = 1.482602 / np.sqrt(6.) * np.nanmedian(np.abs(2.*flux - \
+           np.roll(flux, 2, axis=axis) - np.roll(flux, -2, axis=axis)), \
+           axis=axis)
+    return signal, noise, signal / noise
