e8e93f505f9e596135d7b31c7d0416ba1d3a489e
cebarbosa <carlos.barbosa@usp.br>
2019-04-03 18:40:49 -0700

    Adapted routine to run as a pipeline.

--- SPINS/make_voronoi.py
+++ SPINS/make_voronoi.py
@@ -12,15 +12,17 @@ Apply Voronoi tesselation to WIFIS datacubes.
 from __future__ import print_function, division
 
 import os
+import yaml
 
 import numpy as np
 import astropy.units as u
 from astropy.io import fits
 from astropy.table import Table
+import matplotlib.pyplot as plt
 from vorbin.voronoi_2d_binning import voronoi_2d_binning
 
 import context
-from misc import snr
+import misc
 
 def make_voronoi(data, targetSN, redo=False):
     """ Determination of SNR for each spaxel.
@@ -42,13 +44,13 @@ def make_voronoi(data, targetSN, redo=False):
         1: Voronoi tesselation map
         2: Table with information about the tesselation
     """
-    output = "voronoi_sn{}.fits".format(targetSN)
+    output = os.path.join(os.getcwd(), "voronoi.fits")
     if os.path.exists(output) and not redo:
         return
-    signal, noise, sn = snr(data)
+    signal, noise, sn = misc.snr(data)
     zdim, ydim, xdim = data.shape
     xx, yy = np.meshgrid(np.arange(xdim), np.arange(ydim))
-    # Selecting only saxels where the percentage of nans is low
+    # Selecting only spaxels where the percentage of nans is low
     nnans = np.isnan(data).sum(axis=0)
     mask = np.where(nnans < 80, 0, 1)
     # Preparing arrays for Voronoi binning
@@ -74,7 +76,7 @@ def make_voronoi(data, targetSN, redo=False):
     hdulist = fits.HDUList([vorHDU, tabHDU])
     hdulist.writeto(output, overwrite=True)
 
-def combine_spectra(data, error, header, targetSN, redo=False):
+def combine_spectra(data, header, redo=False, error=None):
     """ Produces the combined spectra for a given binning file.
 
     Input Parameters
@@ -97,53 +99,52 @@ def combine_spectra(data, error, header, targetSN, redo=False):
     a folder called spec_sn{targetSN}.
 
     """
-    outdir = os.path.join(os.getcwd(), "spec1d_sn{}".format(targetSN))
+    outdir = os.path.join(os.getcwd(), "combined")
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     wave = ((np.arange(header['NAXIS3']) + 1
              - header['CRPIX3']) * header['CDELT3'] + header['CRVAL3']) * u.m
-    vordata = fits.getdata("voronoi_sn{}.fits".format(targetSN), 0)
+    vordata = fits.getdata("voronoi.fits", 0)
     bins = np.unique(vordata[~np.isnan(vordata)])
-    filelist = []
     for j, bin in enumerate(bins):
         idy, idx = np.where(vordata == bin)
         ncombine = len(idx)
         print("Bin {0} / {1} (ncombine={2})".format(j + 1, bins.size, ncombine))
-        output = os.path.join(outdir, "sn{}_{:04d}.fits".format(targetSN,
-                                                                int(bin)))
+        output = os.path.join(outdir, "spec{:04d}.fits".format(int(bin)))
         if os.path.exists(output) and not redo:
             continue
         specs = data[:,idy,idx]
-        errors = error[:,idy,idx]
-        errs = np.sqrt(np.nansum(errors**2, axis=1))
         combined = np.nanmean(specs, axis=1) * ncombine
         combined[np.isnan(combined)] = 0
-        mask = np.where(combined==0, 0., 1.)
+        mask = np.where(combined == 0, 0., 1.)
+        if error is not None:
+            errors = error[:,idy,idx]
+            errs = np.sqrt(np.nansum(errors**2, axis=1))
+        else:
+            errs = np.zeros_like(combined)
         table = Table([wave.to("micrometer").data, combined, errs, mask],
                       names=["WAVE", "FLUX", "FLUX_ERR", "MASK"])
         table.write(output, overwrite=True)
-        filelist.append(output)
-    with open("filelist_sn{}.txt".format(targetSN), "w") as f:
-        f.write("\n".join(filelist))
     return
 
 if __name__ == "__main__":
-    wdir = os.path.join(context.data_dir)
-    
-    os.chdir(context.data_dir)
-    # Input
-    targetSN = 40
-    datacube = "M85_combined_cube_2.fits"
-    errcube = "M85_unc_cube.fits"
-    # Reading data
-    data = fits.getdata(datacube)
-    errdata = fits.getdata(errcube)
-    errdata = errdata.sum(axis=0)# Removing extra dimension
-    # Normalizing by the exposure time
-    header = fits.getheader(datacube)
-    exptime = header["OBJTIME"]
-    data /=  exptime
-    errdata /= exptime
-    # Processing data
-    make_voronoi(data, targetSN, redo=False)
-    combine_spectra(data, errdata, header, targetSN, redo=True)
+    wdir = os.path.join(context.data_dir, "WIFIS")
+    for gal in os.listdir(wdir):
+        os.chdir(os.path.join(wdir, gal))
+        config_file = "sn30_mom2.yaml"
+        if config_file not in os.listdir("."):
+            continue
+        with open(config_file) as f:
+            params = yaml.load(f)
+        config = yaml.load(config_file)
+        # Reading data
+        data = fits.getdata(params["datacube"])
+        header = fits.getheader(params["datacube"])
+        # Moving to new working directory
+        outdir = os.path.join(wdir, gal, config_file.replace(".yaml", ""))
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        os.chdir(outdir)
+        # Processing data
+        make_voronoi(data, params["vorSN"], redo=False)
+        combine_spectra(data, header, redo=True)
