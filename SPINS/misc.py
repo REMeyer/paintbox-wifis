ce6a21e5b215f29518f45e0c82702a115b4dd777
cebarbosa <carlos.barbosa@usp.br>
2019-04-05 17:13:34 -0700

    Included routine to read wavelength and spectrum from fits file.

--- SPINS/misc.py
+++ SPINS/misc.py
@@ -9,6 +9,7 @@ Miscelaneous functions.
 
 """
 import numpy as np
+from astropy.io import fits
 
 def calc_wave(header, naxis=1):
     wave = ((np.arange(header['NAXIS3']) + 1
@@ -24,3 +25,17 @@ def snr(flux, axis=0):
            np.roll(flux, 2, axis=axis) - np.roll(flux, -2, axis=axis)), \
            axis=axis)
     return signal, noise, signal / noise
+
+def read_spec(filename, axis=1, extension=0):
+    """ Produces array for wavelenght of a given array. """
+    h = fits.getheader(filename, ext=extension)
+    w0 = fits.getval(filename, "CRVAL{0}".format(axis), extension)
+    if "CD{0}_{0}".format(axis) in list(h.keys()):
+        deltaw = fits.getval(filename, "CD{0}_{0}".format(axis), extension)
+    else:
+        deltaw = fits.getval(filename, "CDELT{0}".format(axis), extension)
+    pix0 = fits.getval(filename, "CRPIX{0}".format(axis), extension)
+    npix = fits.getval(filename, "NAXIS{0}".format(axis), extension)
+    wave = w0 + deltaw * (np.arange(npix) + 1 - pix0)
+    flux = fits.getdata(filename, extension)
+    return wave, flux
